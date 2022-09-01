04ef578f8237aaf1eb1d3cf7498806747178c12e
Alexander Rovang <DXRovang@gmail.com>
2022-09-01 17:09:08 -0400

    adds projects

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_09_01_210037) do
+ActiveRecord::Schema.define(version: 2022_09_01_210802) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -48,6 +48,13 @@ ActiveRecord::Schema.define(version: 2022_09_01_210037) do
     t.text "notes"
     t.string "projects", default: [], array: true
     t.string "materials", default: [], array: true
+  end
+
+  create_table "projects", force: :cascade do |t|
+    t.string "name"
+    t.text "notes"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
 end
